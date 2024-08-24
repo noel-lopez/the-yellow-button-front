ddc89c6abeb92bc4528ca53e859102fa48d957dc
Noel Lopez <nlopez.publi@gmail.com>
2024-08-24 19:17:08 +0200

    chore: hello world with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'pacifico': ['Pacifico', 'ui-sans-serif', 'system-ui']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
